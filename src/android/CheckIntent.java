a701bcf23e7a5aae163b88537fc6743d4aa3a327
FrigoEU <simon.van.casteren@gmail.com>
2017-08-02 11:51:27 +0200

    0.0.2

--- src/android/CheckIntent.java
+++ src/android/CheckIntent.java
@@ -1,4 +1,4 @@
-package org.apache.cordova.plugin;
+package org.caresolutions.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -13,18 +13,19 @@ import android.content.pm;
 */
 public class CheckIntent extends CordovaPlugin {
 
-@Override
-public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    if (action.equals("checkIntent")) {
-      String intent = args.getString(0);
-      PackageManager manager = context.getPackageManager();
-      List<ResolveInfo> infos = manager.queryIntentActivities(intent, 0);
-      if (infos.size() > 0) {
-        callbackContext.success(true);
-      } else {
-        callbackContext.success(false);
+  @Override
+  public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+      if (action.equals("checkIntent")) {
+        String intent = args.getString(0);
+        PackageManager manager = context.getPackageManager();
+        List<ResolveInfo> infos = manager.queryIntentActivities(intent, 0);
+        if (infos.size() > 0) {
+          callbackContext.success(true);
+        } else {
+          callbackContext.success(false);
+        }
+        return true;
       }
-      return true;
-    }
-    return false;
+      return false;
+  }
 }
